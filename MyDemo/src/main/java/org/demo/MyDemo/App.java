b7bc17df7f59e13b8c8a63ce590272b39416242c
kranthi kumar <kranthi kumar@KranthiKumar>
2025-10-22 08:04:17 +0530

    MyDemo Third Version

--- MyDemo/src/main/java/org/demo/MyDemo/App.java
+++ MyDemo/src/main/java/org/demo/MyDemo/App.java
@@ -6,5 +6,6 @@ package org.demo.MyDemo;
 public class App {
     public static void main(String[] args) {
         System.out.println("Hello World!");
-        System.out.println("Happy Diwali") ;    }
+        System.out.println("Happy Diwali") ; 
+        System.out.println("Happy Diwali2") ;}
 }
